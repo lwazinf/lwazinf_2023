ce83927b3fcbe6807747d7ec1063175d41a6f4c9
Lwazi Ndlovu <42185877+lwazinf@users.noreply.github.com>
2023-12-15 21:26:01 +0200

    Update VideoUI_.tsx

--- app/components/ui/VideoUI_.tsx
+++ app/components/ui/VideoUI_.tsx
@@ -165,7 +165,7 @@ export default function VideoUI_() {
       >
         <video controls={false} width="640" height="360" id="videoElement" ref={videoRef}>
           <source
-            src={`https://vod-progressive.akamaized.net/exp=1702639744~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2433%2F26%2F662169694%2F3044888239.mp4~hmac=609c836fbce6f3ba4c85cf3fb619d6095c305200dcb2dec53c5af4587aa28417/vimeo-prod-skyfire-std-us/01/2433/26/662169694/3044888239.mp4?download=1&filename=pexels-westarmoney-10739357+%28360p%29.mp4`}
+            src={`/assets/video/stance.mp4`}
             type="video/mp4"
           />
         </video>
