efa0866f50532126db7010fc934ace0f4ea19345
LwaziNF <lwazinf@gmail.com>
2024-04-20 19:10:09 +0200

    fixed hero_

--- app/page.tsx
+++ app/page.tsx
@@ -16,16 +16,11 @@ import Hero_ from "./components/Hero_";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-[1400px] mx-auto flex-col items-center justify-start">
+    <main className="flex min-h-screen min-w-2 mx-auto flex-col items-center justify-start">
       <Hero_/>
-      <div className={`w-full h-[40px] bg-black flex flex-row justify-center items-center absolute top-0 text-white text-[15px] overflow-hidden`}>
-        <FontAwesomeIcon icon={faWarning} className={`text-[15px] text-orange-500`}/>
-        <p className={`mx-2 animate-pulse`}>Welcome to my portfolio, currently in development.</p>
-        <FontAwesomeIcon icon={faWarning} className={`text-[15px] text-orange-500`}/>
-      </div>
-      <Social_ />
       <Focus_ />
       <Options_ />
+      <Social_ />
     </main>
   );
 }
